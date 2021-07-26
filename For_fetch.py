e2137d928fe9ab9e22658cabbf022d6a4b3f8187
DevankJ <joshidevankc@gmail.com>
2021-07-26 10:34:15 +0530

    third commit with fetch file

---
+++ For_fetch.py
@@ -0,0 +1,17 @@
+import mysql.connector as connector
+
+
+
+def fetch_all():
+    conn=connector.connect(host="localhost",user="root",password="1234",database="pythontest")
+    query="select * from user"
+    cur=conn.cursor()
+    cur.execute(query)
+    
+    
+    for column in cur:
+        print(column)
+    # a=pd.Dataframe(column)
+    # print(a)
+
+fetch_all()
