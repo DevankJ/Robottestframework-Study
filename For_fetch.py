6b21d3b1ac9c5ba1f56e59cf90c23f207a984dbf
DevankJ <joshidevankc@gmail.com>
2021-07-28 09:40:51 +0530

    Testing if git is working or not with a sample file

--- For_fetch.py
+++ For_fetch.py
@@ -4,7 +4,7 @@ import mysql.connector as connector
 
 def fetch_all():
     conn=connector.connect(host="localhost",user="root",password="1234",database="pythontest")
-    query="select * from user"
+    query="select * from emp_data"
     cur=conn.cursor()
     cur.execute(query)
     
